1302e4387818cdee95f1a8fa730a63380d155d53
Will <willjcGA@outlook.com>
2022-07-24 16:29:11 +0800

    refactor: added head.ejs, header.ejs and footer.ejs partials

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("landing.ejs");
 });
 
 app.listen(port, () => {
