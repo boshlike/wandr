10c48c50300d096bbf34883dc32309b065aed101
Will <willjcGA@outlook.com>
2022-07-24 17:43:30 +0800

    feat: added route to home dashboard

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.set("view engine", "ejs");
 // Add the middleware to use and static files
 app.use(express.static('public'));
 
+app.get("/home", (req, res) => {
+    res.render("dash/index_country.ejs");
+});
+
 app.get("/", (req, res) => {
     res.render("landing.ejs");
 });
