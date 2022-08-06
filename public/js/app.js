3a025d31a82d844ba891c48ba4a8bd6d9e18d4c0
Will <willjcGA@outlook.com>
2022-08-06 15:41:19 +0800

    feat: added panel object interactivity
    Added interactive panel object logic to app.js to allow user to filter by visited, planne and all

--- public/js/app.js
+++ public/js/app.js
@@ -21,3 +21,46 @@ const fetchData = async (url) => {
     const data = await response.json()
     return data;
 }
+// Toggle panel view in home dashboard
+document.querySelector("#panel-block").onclick = (event) => {
+    // If the user does not click on a panel button then do nothing
+    if (event.target.classList.contains("panel-tabs")) {
+        return;
+    }
+    // If the icon clicked is already active then do nothing
+    if (event.target.classList.contains("is-active")) {
+        return;
+    }
+    // Remove is active from other tabs
+    const panelNavOptions = event.target.parentNode.children;
+    for (let i = 0, len = panelNavOptions.length; i < len; i++) {
+        panelNavOptions[i].classList.remove("is-active")
+    }
+    // Add is active to tab clicked
+    event.target.classList.add("is-active");
+    const targetName =  event.target.text;
+    console.log(targetName)   
+    // Add is-active to the relevant visited, planned or both
+    const panelBlock = document.getElementsByClassName("panel-block");
+    for (let i = 0, len = panelBlock.length; i < len; i++) {
+        switch(targetName) {
+            case "All":
+                panelBlock[i].removeAttribute("style");
+                break;
+            case "Visited":
+                if (panelBlock[i].classList.contains("visited")) {
+                    panelBlock[i].removeAttribute("style");
+                } else {
+                    panelBlock[i].style.display = "none";
+                }
+                break;
+            case "Planned":
+                if (panelBlock[i].classList.contains("planned")) {
+                    panelBlock[i].removeAttribute("style");
+                } else {
+                    panelBlock[i].style.display = "none";
+                }
+                break;
+        }
+    }
+}
